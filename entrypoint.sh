7108e6eca382f972cb3b4251783b37875cf1b1fd
Dmytro Rudzik <drudzik@atlassian.com>
2019-01-24 14:36:09 +0200

    qwe

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 set -eu
 
-mkdir ~/.jira.d
-echo "login: '$JIRA_USER_EMAIL' \nauthentication-method: api-token" > ~/.jira.d/config.yml
-
-cat ~/.jira.d/config.yml
-
-sh -c "/jira-linux-amd64 $*"
+sh -c "/jira-linux-amd64 $* --login='$JIRA_USER_EMAIL'"
