e63ecaad1e9ea3d7c099ee384ca2126fdbe3e887
Dmytro Rudzik <drudzik@atlassian.com>
2019-01-25 16:57:33 +0200

    tidy up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,34 @@
 #!/bin/sh
 set -eu
+CONFIG_FOLDER_PATH = $HOME/.jira.d
+CONFIG_PATH = $CONFIG_FOLDER_PATH/config.yml
+CREDENTIALS_PATH = $CONFIG_FOLDER_PATH/credentials
 
-if [ ! -f $HOME/.jira.d/config.yml ]; then
-    mkdir $HOME/.jira.d
-    touch $HOME/.jira.d/config.yml
+if [ ! -f $CONFIG_PATH ]; then
+    mkdir $CONFIG_FOLDER_PATH
+    touch $CONFIG_PATH
 
     if [ -z $JIRA_USER_EMAIL ]; then
         echo "ERROR: Please set JIRA_USER_EMAIL env"; exit 1
     else
-        echo "login: $JIRA_USER_EMAIL" >> $HOME/.jira.d/config.yml
+        echo "login: $JIRA_USER_EMAIL" >> $CONFIG_PATH
     fi
 
     if [ -z $JIRA_BASE_URL ]; then
         echo "ERROR: Please set JIRA_BASE_URL env"; exit 1
     else
-       echo "endpoint: $JIRA_BASE_URL" >> $HOME/.jira.d/config.yml
+       echo "endpoint: $JIRA_BASE_URL" >> $CONFIG_PATH
     fi
-else
-    echo 'Config file:\n'
-    cat $HOME/.jira.d/config.yml
 fi
 
-if [ -f $HOME/.jira.d/credentials ]; then
-    echo "Loading stored credentials ..."
-    echo $HOME/.jira.d/credentials
-    export $(grep -v '^#' $HOME/.jira.d/credentials | xargs -d '\n')
+if [ -f $CREDENTIALS_PATH ]; then
+    export $(grep -v '^#' $CREDENTIALS_PATH | xargs -d '\n')
 else
     if [ -z $JIRA_API_TOKEN ]; then
         echo "ERROR: Please set JIRA_API_TOKEN env"; exit 1
     else
-        echo "Storing credentials ..."
-        touch $HOME/.jira.d/credentials
-        echo "JIRA_API_TOKEN=$JIRA_API_TOKEN" >> $HOME/.jira.d/credentials
-        cat $HOME/.jira.d/credentials
+        touch $CREDENTIALS_PATH
+        echo "JIRA_API_TOKEN=$JIRA_API_TOKEN" >> $CREDENTIALS_PATH
     fi
 fi
 
