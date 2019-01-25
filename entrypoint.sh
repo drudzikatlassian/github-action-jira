cf1e32a99494a82808e9e736eff297eed0b9b1dd
Dmytro Rudzik <drudzik@atlassian.com>
2019-01-25 14:21:53 +0200

    authable action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
 set -eu
 
-sh -c "/jira-linux-amd64 $* --login='$JIRA_USER_EMAIL' --endpoint='$JIRA_BASE_URL'"
+if [ ! -f /.jira.d/config.yml ]; then
+    mkdir /.jira.d
+    touch /.jira.d/config.yml
+
+    if test -z "$JIRA_USER_EMAIL"; then
+        echo "login:'$JIRA_USER_EMAIL'" >> /.jira.d/config.yml
+    else
+        raise error "Please set JIRA_USER_EMAIL env"
+    fi
+
+    if test -z "$JIRA_BASE_URL"; then
+        echo "endpoint:'$JIRA_BASE_URL'" >> /.jira.d/config.yml
+    else
+        raise error "Please set JIRA_BASE_URL env"
+    fi
+fi
+
+if [ ! -f /.jira.d/credentials]; then
+    if test -z "$JIRA_API_TOKEN"; then
+        touch /.jira.d/credentials
+        echo "JIRA_API_TOKEN='$JIRA_API_TOKEN'" >> /.jira.d/credentials
+    else
+        raise error "Please set JIRA_API_TOKEN env"
+    fi
+else
+    export $(grep -v '^#' /.jira.d/credentials | xargs -d '\n')
+
+sh -c "/jira-linux-amd64 $*"
