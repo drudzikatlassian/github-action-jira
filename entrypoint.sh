1a06fbaf5fc464bce79a98502b6f22ca6c1dd720
Dmytro Rudzik <drudzik@atlassian.com>
2019-01-25 14:36:01 +0200

    debug2

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,13 @@ if [ ! -f /.jira.d/config.yml ]; then
     touch /.jira.d/config.yml
 
     if [ ! -z "$JIRA_USER_EMAIL" ]; then
-        echo "login:'$JIRA_USER_EMAIL'\n" >> /.jira.d/config.yml
+        echo "login: $JIRA_USER_EMAIL" >> /.jira.d/config.yml
     else
         echo "ERROR: Please set JIRA_USER_EMAIL env"; exit 1
     fi
 
     if [ ! -z "$JIRA_BASE_URL" ]; then
-        echo "endpoint:'$JIRA_BASE_URL'\n" >> /.jira.d/config.yml
+        echo "endpoint: $JIRA_BASE_URL" >> /.jira.d/config.yml
     else
         echo "ERROR: Please set JIRA_BASE_URL env"; exit 1
     fi
@@ -21,7 +21,7 @@ fi
 if [ ! -f /.jira.d/credentials]; then
     if [ ! -z "$JIRA_API_TOKEN" ]; then
         touch /.jira.d/credentials
-        echo "JIRA_API_TOKEN='$JIRA_API_TOKEN'" >> /.jira.d/credentials
+        echo "JIRA_API_TOKEN=$JIRA_API_TOKEN" >> /.jira.d/credentials
     else
         echo "ERROR: Please set JIRA_API_TOKEN env"; exit 1
     fi
