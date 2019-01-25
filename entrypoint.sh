af508b2373f922c58376d0b57ccffe576ae78d1f
Dmytro Rudzik <drudzik@atlassian.com>
2019-01-25 14:27:52 +0200

    there is no raise

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,13 @@ if [ ! -f /.jira.d/config.yml ]; then
     if test -z "$JIRA_USER_EMAIL"; then
         echo "login:'$JIRA_USER_EMAIL'" >> /.jira.d/config.yml
     else
-        raise error "Please set JIRA_USER_EMAIL env"
+        echo "ERROR: Please set JIRA_USER_EMAIL env"; exit 1
     fi
 
     if test -z "$JIRA_BASE_URL"; then
         echo "endpoint:'$JIRA_BASE_URL'" >> /.jira.d/config.yml
     else
-        raise error "Please set JIRA_BASE_URL env"
+        echo "ERROR: Please set JIRA_BASE_URL env"; exit 1
     fi
 fi
 
@@ -23,7 +23,7 @@ if [ ! -f /.jira.d/credentials]; then
         touch /.jira.d/credentials
         echo "JIRA_API_TOKEN='$JIRA_API_TOKEN'" >> /.jira.d/credentials
     else
-        raise error "Please set JIRA_API_TOKEN env"
+        echo "ERROR: Please set JIRA_API_TOKEN env"; exit 1
     fi
 else
     export $(grep -v '^#' /.jira.d/credentials | xargs -d '\n')
