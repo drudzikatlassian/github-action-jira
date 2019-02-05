b123804ad978bd55a6ae202b29561b2f165b220f
Dmytro Rudzik <drudzik@atlassian.com>
2019-02-05 18:42:53 +0200

    change workflow

--- .github/main.workflow
+++ .github/main.workflow
@@ -73,5 +73,5 @@ action "actions/action-builder/docker@master" {
 action "Select Jira Issue From" {
   uses = "./select-issue-from"
   needs = ["Jira Cloud Login"]
-  args = "commit message"
+  args = "ref"
 }
--- select-issue-from/entrypoint.js
+++ select-issue-from/entrypoint.js
@@ -1,1 +1,4 @@
-console.log('args here:' + process.argv)
+const _ = require('lodash')
+console.log('args here:' + process.argv[0])
+const githubEvent = require(process.env['GITHUB_EVENT_PATH'])
+console.log(`Extracting string '${process.argv[0]}' from event: ${_.get(githubEvent, process.argv[0])}`)
