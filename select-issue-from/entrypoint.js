093687870bbea02e74b0a56873fffd6ea84c9f1e
Dmytro Rudzik <drudzik@atlassian.com>
2019-02-05 20:52:54 +0200

    get args

--- select-issue-from/entrypoint.js
+++ select-issue-from/entrypoint.js
@@ -1,5 +1,5 @@
 const _ = require('lodash')
-const arg = process.argv[3]
+const arg = process.argv[2]
 console.log('args here:' + arg)
 const githubEvent = require(process.env['GITHUB_EVENT_PATH'])
 console.log(`Extracting string '${arg}' from event: ${_.get(githubEvent, arg)}`)
