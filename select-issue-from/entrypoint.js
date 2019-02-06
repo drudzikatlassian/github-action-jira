330e0c0a0abef3c02801b158df6253dfc7d83c7f
Dmytro Rudzik <drudzik@atlassian.com>
2019-02-06 13:35:30 +0200

    qwe

--- select-issue-from/entrypoint.js
+++ select-issue-from/entrypoint.js
@@ -1,12 +1,18 @@
 const _ = require('lodash')
 const argv = require('minimist')(process.argv.slice(2))
 const githubEvent = require(process.env['GITHUB_EVENT_PATH'])
+const issueIdRegEx = /([a-zA-Z0-9]+-[0-9]+)/
+function getExtractString(args) {
+  if (args.event) {
+    return _.get(githubEvent, args.event)
+  }
 
-let extractString = ''
-console.log(`GithubEvent: ${JSON.stringify(githubEvent, null, 4)}`)
-
-if (argv.event) {
-  extractString = _.get(githubEvent, argv.event)
+  return ''
 }
 
+console.log(`GithubEvent: ${JSON.stringify(githubEvent, null, 4)}`)
+
+const extractString = getExtractString(argv)
 console.log(`Extracting from string '${extractString}'`)
+const match = extractString.match(issueIdRegEx)
+console.log('match:', match)
