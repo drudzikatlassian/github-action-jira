7b72824fc6febd9284e663d58f0add90f9e40bdd
Dmytro Rudzik <drudzik@atlassian.com>
2019-02-07 17:29:09 +0200

    create issue

--- create-issue/CreateIssueAction.js
+++ create-issue/CreateIssueAction.js
@@ -9,6 +9,38 @@ module.exports = class CreateIssue {
   async execute() {
     console.log(`argv:${JSON.stringify(this.argv, null, 4)}`)
     console.log('process.argv:' + JSON.stringify(process.argv, null, 4))
+    const {
+      fields,
+      project,
+      issueType
+    } = this.argv
+
+    const payload = {
+      fields,
+      project,
+      issueType
+    }
+
+    const auth = 'Basic ' + Buffer.from(this.config.email + ':' + this.config.token).toString('base64');
+    const url = `${this.config.baseUrl}/rest/api/3/issue`
+    const result = await fetch(url, { 
+      method: 'POST',
+      headers: {
+        Authorization: auth,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+    
+    console.log(`creating issue status: ${result.ok}`)
+
+    const jsonRes = await result.json()
+    console.log('jsonRes:' + JSON.stringify(jsonRes, null, 4))
+    
+    if (result.ok) {
+      return {issue: jsonRes.id}
+    }
+
     return
   }
 }
