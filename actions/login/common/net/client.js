3312da903debfcbe093eb2afc5a43f9e0c33cb46
Dmytro Rudzik <drudzik@atlassian.com>
2019-02-18 13:21:19 +0200

    cleanup

--- actions/login/common/net/client.js
+++ actions/login/common/net/client.js
@@ -1,23 +1,12 @@
 const fetch = require('node-fetch')
-// const moment = require('moment')
 
 module.exports = serviceName => async (state, apiMethod = 'unknown') => {
-  // const startTime = moment.now()
-
   const response = await fetch(state.req.url, state.req)
 
   state.res = {
     headers: response.headers.raw(),
     status: response.status,
   }
-
-  // const totalTime = moment.now() - startTime
-  // const tags = {
-  //   api_method: apiMethod,
-  //   method: state.req.method || 'GET',
-  //   response_code: response.status,
-  //   service: serviceName,
-  // }
 
   state.res.body = await response.text()
 
--- actions/login/entrypoint.sh
+++ actions/login/entrypoint.sh
@@ -1,32 +1,4 @@
 #!/bin/sh
 set -eu
 
-# mkdir "$HOME/.jira.d"
-# touch "$HOME/.jira.d/config.yml"
-
-# mkdir "$HOME/jira"
-# touch "$HOME/jira/config.yml"
-
-# if [ -z "$JIRA_USER_EMAIL" ]; then
-#     echo "ERROR: Please set JIRA_USER_EMAIL env"; exit 1
-# else
-#     echo "login: $JIRA_USER_EMAIL" >> "$HOME/.jira.d/config.yml"
-#     echo "email: $JIRA_USER_EMAIL" >> "$HOME/jira/config.yml"
-# fi
-
-# if [ -z "$JIRA_BASE_URL" ]; then
-#     echo "ERROR: Please set JIRA_BASE_URL env"; exit 1
-# else
-#     echo "endpoint: $JIRA_BASE_URL" >> "$HOME/.jira.d/config.yml"
-#     echo "baseUrl: $JIRA_BASE_URL" >> "$HOME/jira/config.yml"
-# fi
-
-# if [ -z "$JIRA_API_TOKEN" ]; then
-#     echo "ERROR: Please set JIRA_API_TOKEN env"; exit 1
-# else
-#     touch "$HOME/.jira.d/credentials"
-#     echo "JIRA_API_TOKEN=$JIRA_API_TOKEN" >> "$HOME/.jira.d/credentials"
-#     echo "token: $JIRA_API_TOKEN" >> "$HOME/jira/config.yml"
-# fi
-
 sh -c "node /index.js $*"
