d7d09816c2087e01746d8768aa3b5e51114babfa
Dmytro Rudzik <drudzik@atlassian.com>
2019-02-18 13:02:46 +0200

    use node to make an login

---
+++ actions/login/action.js
@@ -0,0 +1,23 @@
+const _ = require('lodash')
+const Jira = require('./common/net/Jira')
+
+module.exports = class {
+  constructor ({ githubEvent, argv, config }) {
+    this.Jira = new Jira({
+      baseUrl: config.baseUrl,
+      token: config.token,
+      email: config.email,
+    })
+
+    this.config = config
+    this.argv = argv
+    this.githubEvent = githubEvent
+  }
+
+  async execute () {
+    
+    const myself = await this.Jira.getMyself()
+    console.log('myself:' + JSON.stringify(myself, null, 4))
+    return this.config
+  }
+}
