ab289d9c6ad868264181392b5044b1b1b2ee817d
Dmytro Rudzik <drudzik@atlassian.com>
2019-02-22 16:59:57 +0200

    use sha1sum

--- actions/cli/entrypoint.sh
+++ actions/cli/entrypoint.sh
@@ -6,7 +6,7 @@ export $(grep -v '^#' "$HOME/.jira.d/credentials" | xargs -d '\n')
 sh -c "/jira-linux-amd64 $*"
 
 actionSubjectId="cli"
-containerId=`echo $GITHUB_REPOSITORY | sha1sum -a 256 | cut -c1-65`
-anonymousId=`echo $GITHUB_ACTOR | sha1sum -a 256 | cut -c1-65`
+containerId=`echo $GITHUB_REPOSITORY | sha1sum | cut -c1-41`
+anonymousId=`echo $GITHUB_ACTOR | sha1sum | cut -c1-41`
 
 /gagas --container-id="$containerId" --action-subject-id="$actionSubjectId" --anonymous-id="$anonymousId"
