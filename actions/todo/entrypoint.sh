a5b425704768d1bd4405aeff30dd45b6934480b8
Dmytro Rudzik <drudzik@atlassian.com>
2019-02-22 16:46:00 +0200

    shasum -> sha1sum

--- actions/todo/entrypoint.sh
+++ actions/todo/entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 sh -c "node /index.js $*"
 
 actionSubjectId="todo"
-containerId=`echo $GITHUB_REPOSITORY | shasum -a 256 | cut -c1-65`
-anonymousId=`echo $GITHUB_ACTOR | shasum -a 256 | cut -c1-65`
+containerId=`echo $GITHUB_REPOSITORY | sha1sum -a 256 | cut -c1-65`
+anonymousId=`echo $GITHUB_ACTOR | sha1sum -a 256 | cut -c1-65`
 
 /gagas --container-id="$containerId" --action-subject-id="$actionSubjectId" --anonymous-id="$anonymousId"
